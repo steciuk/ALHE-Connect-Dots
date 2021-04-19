b4d0a3f78e87e6319a80a5ba507e07a794195519
Repumba <75318192+Repumba@users.noreply.github.com>
2021-04-19 23:01:50 +0200

    Add files via upload

--- regresja.py
+++ regresja.py
@@ -1,14 +1,51 @@
 import pandas as pd
 import numpy as np
+#import sympy
 
-def calc_regression(midpoints): #oblicza regresje dla ostatniego parametru
+def calc_regression(midpoints, scores=None): #oblicza regresje dla ostatniego parametru lub dla wyników, jeśli podane
+    if scores != None:
+        for i in range( len(midpoints) ):
+            midpoints[i].append(scores[i])
     df = pd.DataFrame(midpoints)
     X = df.iloc[:, :-1]  # wyciaga wartosci, na podstawie ktorych jest estymacja
     Y = df.iloc[:, -1]  # wyciaga wartosci, do ktorych chce sie dopacowac
     lin_reg = LinearRegression()
     lin_reg.fit(X, Y)
-    #Y_pred = lin_reg.predict(X)
-    #lin_reg.coef_
+    return lin_reg #zwraca klase LinearRegression()
+
+    #return (lin_reg.coef_).tolist(), lin_reg.intercept_  #zwraca parametry regresji liniowej oraz bazową warość 
+
+
+def prep_for_sympy(poly):
+    s = ""
+    stopien = len(poly) - 1
+    for p in poly:
+        s += str(p)
+        if stopien>1:
+            s += "*x**" + str(stopien) + " + "
+        elif stopien==1:
+            s += "*x + "
+        stopien -= 1
+    return s
+
+def solve_polynomial(poly):  #znajduje miejsca zerowe wielomianu
+    x = sympy.symbols('x')
+    return sympy.solve(prep_for_sympy(poly), x)
+
+def calc_der_for_each(midpoints, scores): #liczy pochodna dla kazdego parametru osobno
+    df = pd.DataFrame(midpoints)
+    sz = df.shape[1]-1
+    ders = []
+    for i in range(df.shape[1]):
+        f = numpy.polyfit(df[i].tolist(), df[sz].tolist(), 3) #funkcja 3-ego stopnia
+        f.reverse()
+        for j in range(len(f)-1):
+            f[j] = f[j+1]*(j+1)
+        f.reverse()
+        der.append(f)
+    return der
+
+###Funkcje poniżej robia predykcje współrzędnej punktu - być może zaszłaby taka potrzeba, gdyby jakichś danych brakowało
 
 def calc_reg_for_each(midpoints): #robi regresje dla kazdego parametru oddzielnie
     params = []
