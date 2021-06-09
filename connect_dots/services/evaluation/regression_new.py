c6aeaf7acd06f0c2534f894ff3dd82fb6dcde941
ttrzecia <t.trzeciak@stud.elka.pw.edu.pl>
2021-06-09 12:25:38 +0200

    regression_new update - one plot for all

--- connect_dots/services/evaluation/regression_new.py
+++ connect_dots/services/evaluation/regression_new.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 import services.evaluation.regression
+from services.evaluation.distance import calc_distance
 from utils.plotter import simplePlot
 
 
-def regression_correct(mid_points, maximum, dim):
+def regression_correct(mid_points, maximum, dim, plot_for_each=True):
     """
     Predicts optimum using linear regression over the percentage of discarded points
     The function evaluates the prediction and draws plots
     :param mid_points: list of points
     :param maximum: true optimum, parameter used for evaluation of the prediction
     :param dim: number of dimensions
+    :param plot_for_each: True if draw plot for every dimension, False if draw only one main plot
     """
     optimum = []
     predict_list = []  # list of predicted values for each dim, in every iteration
+    if dim > 7:
+        plot_for_each=False
 
     for d in range(dim):
         percentages = [1]
@@ -25,15 +29,24 @@ def regression_correct(mid_points, maximum, dim):
         optimum.append(predict_list[-1][-1])
 
     predict_list = np.array(predict_list)
-    for d in range(dim):
+    if plot_for_each:
+        for d in range(dim):
+            pred_dist = []
+            for m in range(len(predict_list[0])):
+                pred_dist.append(np.abs(predict_list[d, m] - maximum[d]))
+            simplePlot(pred_dist, pltTitle="Odleglosc od optimum w wymiarze " + str(d + 1) +
+                                           " (regresja po % odrzuconych)", pltName='pred_dist_in_dim' + str(d))
+    else:
         pred_dist = []
         for m in range(len(predict_list[0])):
-            pred_dist.append(np.abs(predict_list[d, m] - maximum[d]))
-        simplePlot(pred_dist, pltTitle="Odleglosc od optimum w wymiarze " + str(d + 1) + " (regresja po % odrzuconych)",
-                   pltName='pred_dist_in_dim' + str(d))
+            point = predict_list[:, m].tolist()
+            pred_dist.append(calc_distance(point, maximum))
+        simplePlot(pred_dist, pltTitle="Odleglosc predykcji od optimum (regresja po % odrzuconych)",
+                   pltName="pred_dist_global")
     return optimum
 
-def mean_correct(mid_points, maximum, dim):
+
+def mean_correct(mid_points, maximum, dim, plot_for_each=True):
     """
     Predicts optimum using averaging given points
     The function evaluates the prediction and draws plots
@@ -43,6 +56,8 @@ def mean_correct(mid_points, maximum, dim):
     """
     optimum = []
     mean_predict_list = []  # list of predicted values for each dim, in every iteration
+    if dim > 7:
+        plot_for_each=False
 
     for d in range(dim):
         percentages = [1]
@@ -53,11 +68,18 @@ def mean_correct(mid_points, maximum, dim):
         optimum.append(mean_predict_list[-1][-1])
 
     mean_predict_list = np.array(mean_predict_list)
-    for d in range(dim):
-        mean_dist = []
+    if plot_for_each:
+        for d in range(dim):
+            mean_dist = []
+            for m in range(len(mean_predict_list[0])):
+                mean_dist.append(np.abs(mean_predict_list[d, m] - maximum[d]))
+            simplePlot(mean_dist, pltTitle="Odleglosc od optimum w wymiarze " + str(d + 1) + " (usrednienie midpointow)",
+                       pltName='mean_dist_in_dim' + str(d))
+    else:
+        mean_pred_dist = []
         for m in range(len(mean_predict_list[0])):
-            mean_dist.append(np.abs(mean_predict_list[d, m] - maximum[d]))
-        simplePlot(mean_dist, pltTitle="Odleglosc od optimum w wymiarze " + str(d + 1) + " (usrednienie midpointow)",
-                   pltName='mean_dist_in_dim' + str(d))
-
+            point = mean_predict_list[:, m].tolist()
+            mean_pred_dist.append(calc_distance(point, maximum))
+        simplePlot(mean_pred_dist, pltTitle="Odleglosc predykcji od optimum (usrednienie midpointow)",
+                   pltName="mean_pred_dist_global")
     return optimum
